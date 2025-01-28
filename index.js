ae5e4f9f6ae9e6898f2a8aabc202275a66078b86
Oussama <Messamessus@yahoo.com>
2025-01-28 17:58:07 +0100

    update backend

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173' ,"https://book-app-frontend-lime.vercel.app/"],
+    origin: ['https://book-app-frontend-lime.vercel.app/'],
     credentials: true
 }))
 
